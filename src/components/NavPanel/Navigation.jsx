6f19413c1deb443edcf817e92d83df7ab04d308b
Karan Joshi <Karan190213@gmail.com>
2024-03-21 16:54:09 +0530

    update the nav bar

--- src/components/NavPanel/Navigation.jsx
+++ src/components/NavPanel/Navigation.jsx
@@ -1,24 +1,35 @@
-import React from 'react';
 import { Link } from 'react-router-dom';
-import './Navigation.css';
+import { useState } from 'react';
 
 const Navigation = () => {
+    const [isOpen, setIsOpen] = useState(false);
+
     return (
-        <nav className="navigation">
-            <ul>
-                <li>
-                    <Link to="/">Books</Link>
-                </li>
-                <li>
-                    <Link to="/favorites">My Favorites</Link>
-                </li>
-                <li>
-                    <Link to="/add">Add Book</Link>
-                </li>
-                <li>
-                    <Link to="/delete">Delete Book</Link>
-                </li>
-            </ul>
+        <nav className="flex items-center justify-between flex-wrap bg-gray-500 p-6">
+            <div className="flex items-center flex-shrink-0 text-white mr-6">
+                <span className="font-semibold text-xl tracking-tight">Bookify</span>
+            </div>
+            <div className="block lg:hidden">
+                <button onClick={() => setIsOpen(!isOpen)} className="flex items-center px-3 py-2 border rounded text-gray-200 border-gray-400 hover:text-white hover:border-white">
+                    <svg className="fill-current h-3 w-3" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><title>Menu</title><path d="M0 3h20v2H0V3zm0 6h20v2H0V9zm0 6h20v2H0v-2z"/></svg>
+                </button>
+            </div>
+            <div className={`${isOpen ? 'block' : 'hidden'} w-full block flex-grow lg:flex lg:items-center lg:w-auto`}>
+                <div className="text-sm lg:flex-grow">
+                    <Link to="/booklist" className="block mt-4 lg:inline-block lg:mt-0 text-gray-200 hover:text-white mr-4">
+                        Books
+                    </Link>
+                    <Link to="/favorites" className="block mt-4 lg:inline-block lg:mt-0 text-gray-200 hover:text-white mr-4">
+                        My Favorites
+                    </Link>
+                    <Link to="/add" className="block mt-4 lg:inline-block lg:mt-0 text-gray-200 hover:text-white mr-4">
+                        Add Book
+                    </Link>
+                    <Link to="/delete" className="block mt-4 lg:inline-block lg:mt-0 text-gray-200 hover:text-white">
+                        Delete Book
+                    </Link>
+                </div>
+            </div>
         </nav>
     );
 };
