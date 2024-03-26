8aae02b04a8fa303f23bc624a71c73794de8335f
ometman <revomet@gmail.com>
2024-03-26 12:46:31 +0000

    Modify userActions.js and bookReducer.js to get isAdded state value

--- src/reducers/bookReducer.js
+++ src/reducers/bookReducer.js
@@ -1,9 +1,11 @@
-const bookReducer = (state = { list: [], selectedBook: null }, action) => {
+const bookReducer = (state = { list: [], isAdded: false, selectedBook: null }, action) => {
   switch (action.type) {
     case 'FETCH_BOOKS':
       return { ...state, list: action.payload };
     case 'FETCH_BOOK':
       return { ...state, selectedBook: action.payload };
+    case 'ADD_BOOK':
+      return { ...state, isAdded: true, list: [...state.list, action.payload] }; 
     default:
       return state;
   }
