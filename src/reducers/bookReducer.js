f03ed3161032cd212f377b562c56e21755543b4f
ometman <revomet@gmail.com>
2024-03-26 22:59:55 +0000

    Add action type to book reducer to fetch user-specific books

--- src/reducers/bookReducer.js
+++ src/reducers/bookReducer.js
@@ -1,4 +1,4 @@
-const bookReducer = (state = { list: [], error: null, isAdded: false, selectedBook: null }, action) => {
+const bookReducer = (state = { list: [], userBooks: [], error: null, isAdded: false, selectedBook: null }, action) => {
   switch (action.type) {
     case 'FETCH_BOOKS':
       return { ...state, list: action.payload };
@@ -8,6 +8,10 @@ const bookReducer = (state = { list: [], error: null, isAdded: false, selectedBo
       return { ...state, isAdded: true, error: null, list: [...state.list, action.payload] };
     case 'ADD_BOOK_FAILURE':
       return { ...state, error: action.payload };
+    case 'DELETE_BOOK_FETCH_SUCCESS':
+      return { ...state, userBooks: action.payload };
+    case 'DELETE_BOOK_FETCH_FAILURE':
+    return { ...state, error: action.payload};
     default:
       return state;
   }
