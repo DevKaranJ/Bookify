f8a372a74f5919f56fb478b4af453191f9835644
Karan Joshi <Karan190213@gmail.com>
2024-03-27 20:32:42 +0530

    added the signup button to the login page

--- src/components/auth/Login.jsx
+++ src/components/auth/Login.jsx
@@ -72,6 +72,14 @@ const Login = () => {
               </span>
               Sign in
             </button>
+
+            <button
+  type="button"
+  className="group relative w-full flex justify-center py-2 px-4 border border-transparent text-sm font-medium rounded-md text-white bg-blue-600 hover:bg-blue-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-blue-500 mt-4"
+  onClick={() => navigate('/signup')}
+>
+  Sign Up
+</button>
           </div>
         </form>
       </div>
